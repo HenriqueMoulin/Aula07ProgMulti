6631766bfeb67979ba9812876949c2517534233c
HenriqueMoulin <henri.moulin@live.com>
2018-11-04 16:10:33 -0200

    Aula07ProgMulti

---
+++ entrega_prog_multi_aula07/src/model/model_filme.ts
@@ -0,0 +1,13 @@
+/**
+ *  Nome: Jônatas Ferreira de Oliveira RA: 818231518
+ */
+
+export interface Filme {
+    nome: string;
+    descricao: string;
+}
+
+export interface Genero {
+    nome: string;
+    genero: string;
+}
